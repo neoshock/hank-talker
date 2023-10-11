411a25819d2ee66a1a7c2edcf41cadea763b56d2
Heydilv <101762685+Heydilv@users.noreply.github.com>
2023-10-10 22:09:24 -0500

    login

--- lib/features/login/pages/login_page.dart
+++ lib/features/login/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hank_talker_mobile/features/login/pages/register_page.dart';
 import 'package:hank_talker_mobile/widgets/buttons.dart';
 import 'package:hank_talker_mobile/widgets/inputs.dart';
 import 'package:flutter_phosphor_icons/flutter_phosphor_icons.dart';
@@ -15,7 +16,7 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   final formGlobalKey = GlobalKey<FormState>();
-
+  bool showPassword = false;
   void login() {
     if (formGlobalKey.currentState!.validate()) {
       showDialog(
@@ -42,15 +43,31 @@ class _LoginPageState extends State<LoginPage> {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
+                        // Agrega la imagen centrada en la parte superior
+                        Center(
+                          child: Container(
+                            margin: EdgeInsets.only(
+                                bottom:
+                                    20), // Ajusta el margen inferior según sea necesario
+                            child: Image.network(
+                              'https://cdn-icons-png.flaticon.com/512/5687/5687730.png', // Ruta de la imagen en tus assets
+                              width:
+                                  150, // Ajusta el ancho de la imagen según sea necesario
+                              height:
+                                  150, // Ajusta la altura de la imagen según sea necesario
+                            ),
+                          ),
+                        ),
+
                         CustomTextImputWithLabel(
-                          'Email',
+                          'Correo electrónico',
                           emailController,
                           TextInputType.emailAddress,
                           context,
                           const Icon(PhosphorIcons.envelope),
                           (value) {
                             if (value == null || value == '') {
-                              return 'Email is required';
+                              return 'Correo electrónico es requerido';
                             }
                             return null;
                           },
@@ -61,14 +78,45 @@ class _LoginPageState extends State<LoginPage> {
                             TextInputType.visiblePassword,
                             context,
                             const Icon(PhosphorIcons.lock),
-                            'Password', (value) {
+                            'Contraseña', (value) {
                           if (value == null || value == '') {
-                            return 'Password is required';
+                            return 'Contraseña es requerida';
                           }
                           return null;
-                        }, false),
+                        }, () {
+                          setState(() {
+                            showPassword = !showPassword;
+                          });
+                        }, showPassword),
                         const SizedBox(height: 15),
-                        CusttomButtonRounded(context, () => login(), 'Login'),
+                        CusttomButtonRounded(
+                            context, () => login(), 'Iniciar Sesión'),
+                        const SizedBox(height: 60),
+                        GestureDetector(
+                          onTap: () {
+                            //  print("Registrarse Tocado"); // ejecutando
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RegisterPage()),
+                            );
+                          },
+                          child: RichText(
+                            text: TextSpan(
+                              text: '¿No tienes una cuenta? ',
+                              style: TextStyle(color: Colors.black),
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: 'Registrarse',
+                                  style: TextStyle(
+                                    color: Colors.green,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
                       ],
                     )))));
   }
---
+++ lib/features/login/pages/register_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:hank_talker_mobile/widgets/buttons.dart';
+import 'package:hank_talker_mobile/widgets/inputs.dart';
+
+class RegisterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
