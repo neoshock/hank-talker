d16af44d65b1de63edaf4de91148be7627511d8e
Josue Alcivar <pideun@gmail.com>
2023-10-13 21:19:37 -0500

    implement profile page

--- lib/features/profile/pages/profile_page.dart
+++ lib/features/profile/pages/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hank_talker_mobile/features/profile/widgets/curved_background.dart';
+import 'package:hank_talker_mobile/features/profile/widgets/profile_card_header.dart';
 import 'package:hank_talker_mobile/features/settings/pages/settings_page.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -11,16 +13,21 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SettingsPage()),
-                );
-              },
-              child: Text('Settings'))),
-    );
+    return SafeArea(
+        top: false,
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height * 0.45,
+                ),
+                CurvedBackground(),
+                Positioned(bottom: 0, child: ProfileCardHeader())
+              ],
+            )
+          ],
+        ));
   }
 }
--- lib/features/settings/pages/settings_page.dart
+++ lib/features/settings/pages/settings_page.dart
@@ -7,132 +7,137 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        //padding: const EdgeInsets.symmetric(horizontal: 40),
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-          children: [
-            SizedBox(height: 30),
-            Container(
-              alignment: Alignment.centerLeft,
-              child: Row(
-                children: [
-                  CustomBackButton(context, () {
-                    Navigator.pop(context);
-                  }),
-                  SizedBox(width: 95), // Espacio entre el botón y el texto
-                  Text(
-                    'Ajustes',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text(
+            'Ajustes',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          leadingWidth: 76,
+          leading: Container(
+            margin: const EdgeInsets.only(left: 15),
+            padding: const EdgeInsets.all(3),
+            child: CustomBackButton(context, () {
+              Navigator.pop(context);
+            }),
+          ),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+          child: Padding(
+            //padding: const EdgeInsets.symmetric(horizontal: 40),
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              children: [
+                const SizedBox(
+                    height: 30), // Espacio entre el texto y el primer ListTile
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white, // Fondo blanco
+                    border: Border.all(
+                      color: const Color.fromARGB(
+                          255, 213, 231, 223), // Color del borde gris claro
+                      width: 1.0, // Ancho del borde
+                    ),
+                    borderRadius: const BorderRadius.all(
+                        Radius.circular(10)), // Bordes redondeados
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 6.0),
+                    child: ListTile(
+                      //tileColor:
+                      //    Colors.transparent, // Fondo del ListTile transparente
+                      title: const Text(
+                        'País',
+                        style: TextStyle(fontSize: 12, color: Colors.grey),
+                      ),
+                      subtitle: Row(
+                        children: [
+                          const SizedBox(height: 50),
+                          Image.asset(
+                            'assets/images/ecuador_logo.png', // Ruta de la imagen de Ecuador
+                            width: 40,
+                            height: 40,
+                          ),
+                          const SizedBox(
+                              width: 20), // Espaciado entre el logo y el texto
+                          const Text(
+                            'Ecuador',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      trailing: const Icon(Icons.arrow_forward),
+                      onTap: () {
+                        // Acción cuando se hace clic en este ListTile
+                      },
                     ),
                   ),
-                ],
-              ),
-            ),
-            SizedBox(height: 30), // Espacio entre el texto y el primer ListTile
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white, // Fondo blanco
-                border: Border.all(
-                  color: const Color.fromARGB(
-                      255, 213, 231, 223), // Color del borde gris claro
-                  width: 1.0, // Ancho del borde
                 ),
-                borderRadius:
-                    BorderRadius.all(Radius.circular(10)), // Bordes redondeados
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 6.0),
-                child: ListTile(
-                  //tileColor:
-                  //    Colors.transparent, // Fondo del ListTile transparente
-                  title: Text(
-                    'País',
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                  subtitle: Row(
-                    children: [
-                      SizedBox(height: 50),
-                      Image.asset(
-                        'assets/images/ecuador_logo.png', // Ruta de la imagen de Ecuador
-                        width: 40,
-                        height: 40,
-                      ),
-                      SizedBox(width: 20), // Espaciado entre el logo y el texto
-                      Text(
-                        'Ecuador',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ],
-                  ),
-                  trailing: Icon(Icons.arrow_forward),
-                  onTap: () {
-                    // Acción cuando se hace clic en este ListTile
-                  },
-                ),
-              ),
-            ),
 
-            SizedBox(height: 40), // Espacio entre el primer y segundo ListTile
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white, // Fondo blanco
-                border: Border.all(
-                  color: const Color.fromARGB(
-                      255, 213, 231, 223), // Color del borde gris claro
-                  width: 1.0, // Ancho del borde
-                ),
-                borderRadius:
-                    BorderRadius.all(Radius.circular(10)), // Bordes redondeados
-              ),
-              child: Padding(
-                padding:
-                    const EdgeInsets.only(top: 8.0), // Agrega espacio superior
-                child: ListTile(
-                  title: Text(
-                    'Otros Ajustes',
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                const SizedBox(
+                    height: 40), // Espacio entre el primer y segundo ListTile
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white, // Fondo blanco
+                    border: Border.all(
+                      color: const Color.fromARGB(
+                          255, 213, 231, 223), // Color del borde gris claro
+                      width: 1.0, // Ancho del borde
+                    ),
+                    borderRadius: const BorderRadius.all(
+                        Radius.circular(10)), // Bordes redondeados
                   ),
-                  subtitle: Column(
-                    children: [
-                      ListTile(
-                        title: Text('Notificaciones'),
-                        trailing: Icon(Icons.arrow_forward),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => LoginPage()),
-                          );
-                        },
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 8.0), // Agrega espacio superior
+                    child: ListTile(
+                      title: const Text(
+                        'Otros Ajustes',
+                        style: TextStyle(fontSize: 12, color: Colors.grey),
                       ),
-                      ListTile(
-                        title: Text('Política de Privacidad'),
-                        trailing: Icon(Icons.arrow_forward),
-                        onTap: () {
-                          // Acción cuando se hace clic en "Política de Privacidad"
-                        },
+                      subtitle: Column(
+                        children: [
+                          ListTile(
+                            title: const Text('Notificaciones'),
+                            trailing: const Icon(Icons.arrow_forward),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const LoginPage()),
+                              );
+                            },
+                          ),
+                          ListTile(
+                            title: const Text('Política de Privacidad'),
+                            trailing: const Icon(Icons.arrow_forward),
+                            onTap: () {
+                              // Acción cuando se hace clic en "Política de Privacidad"
+                            },
+                          ),
+                          ListTile(
+                            title: const Text('Terminos y Condiciones'),
+                            trailing: const Icon(Icons.arrow_forward),
+                            onTap: () {
+                              // Acción cuando se hace clic en "Terminos y Condiciones"
+                            },
+                          ),
+                        ],
                       ),
-                      ListTile(
-                        title: Text('Terminos y Condiciones'),
-                        trailing: Icon(Icons.arrow_forward),
-                        onTap: () {
-                          // Acción cuando se hace clic en "Terminos y Condiciones"
-                        },
-                      ),
-                    ],
+                      onTap: () {
+                        // Acción cuando se hace clic en "Otros Ajustes"
+                      },
+                    ),
                   ),
-                  onTap: () {
-                    // Acción cuando se hace clic en "Otros Ajustes"
-                  },
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
