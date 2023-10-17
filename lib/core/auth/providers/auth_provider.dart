8453fc84e6fc89753b0f08c4abd62e2bea7e48f9
CristopherZambrano <79436828+CristopherZambrano@users.noreply.github.com>
2023-10-17 16:06:11 -0500

    No me acuerdo que hice

--- lib/core/auth/providers/auth_provider.dart
+++ lib/core/auth/providers/auth_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hank_talker_mobile/core/auth/services/auth_service.dart';
 import 'package:hank_talker_mobile/core/repositories/http_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthProvider with ChangeNotifier {
   final AuthService authService = AuthService();
@@ -10,7 +11,9 @@ class AuthProvider with ChangeNotifier {
 
   Future<HttpBaseResponde> login(String email, String password) async {
     final respose = await authService.login(email, password);
+    final prefs = await SharedPreferences.getInstance();
     if (respose.code == 200) {
+      await prefs.setString('token', respose.data['token'].toString());
       _isAuth = true;
     } else {
       _isAuth = false;
